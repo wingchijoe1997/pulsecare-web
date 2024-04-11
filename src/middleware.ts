4c396b30df2df0a10e83b424dbf4a7238297bf26
David Pietrocola <pietrocoladavid@gmail.com>
2024-04-11 15:49:02 -0400

    feat(middleware): adds TODO for future work

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,12 +6,20 @@ import {
   authRoutes,
   DEFAULT_LOGIN_REDIRECT,
 } from "@/lib/routes";
-import { auth } from "./auth";
 const { auth: middleware } = NextAuth(authConfig);
 
 export default middleware(async (req) => {
   const { nextUrl } = req;
+  // TODO: understand if it's better to check req.auth or auth() in the middleware
   const isLoggedIn = !!req.auth;
+  // const isLoggedIn = !!await auth();
+
+  // const session = await auth();
+  // const user1 = session?.user;
+  // console.log("user role in session in middleware", user1?.role);
+
+  // const user2 = req.auth?.user;
+  // console.log("user role in req.auth in middleware", user2?.role);
 
   const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
   const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
@@ -20,21 +28,12 @@ export default middleware(async (req) => {
   if (isApiAuthRoute) {
     return;
   }
-  const session = await auth();
-  const user1 = session?.user;
-  console.log("user role in session in middleware", user1?.role);
-
-  const user2 = req.auth?.user;
-  console.log("user role in req.auth in middleware", user2?.role);
 
   // When the user visits the auth route
   if (isAuthRoute) {
     // If it's already logged in we redirect them
     if (isLoggedIn) {
-      const role = session?.user.role;
-      return Response.redirect(
-        new URL(role ? "/dashboard" : DEFAULT_LOGIN_REDIRECT, nextUrl.origin),
-      );
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl.origin));
     }
     // Otherwise remain in the login page
     return;
