25b73f3eb5d0fe4ec76533f525c66e5c27ac2a63
David Pietrocola <pietrocoladavid@gmail.com>
2024-04-13 12:04:26 -0400

    refactor: moves apollo provider file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import NextTopLoader from "nextjs-toploader";
-import { ApolloProviders } from "./providers/apollo-provider";
+import { ApolloProviders } from "../providers/apollo-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
